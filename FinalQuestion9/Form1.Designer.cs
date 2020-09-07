eb0f60b8bc99bcdd961252904fd4527a57c716ce
KayakingBeechMarten <60995281+KayakingBeechMarten@users.noreply.github.com>
2020-09-07 00:00:41 -0700

    Add files via upload

---
+++ FinalQuestion9/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace FinalQuestion9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ohmbg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.band1 = new System.Windows.Forms.Label();
+            this.band5 = new System.Windows.Forms.Label();
+            this.band2 = new System.Windows.Forms.Label();
+            this.band3 = new System.Windows.Forms.Label();
+            this.band4 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog3 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog4 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog5 = new System.Windows.Forms.ColorDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ohmbg
+            // 
+            this.ohmbg.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ohmbg.Location = new System.Drawing.Point(481, 194);
+            this.ohmbg.Name = "ohmbg";
+            this.ohmbg.Size = new System.Drawing.Size(190, 92);
+            this.ohmbg.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.label1.Location = new System.Drawing.Point(630, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 114);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.NavajoWhite;
+            this.label2.Location = new System.Drawing.Point(462, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 114);
+            this.label2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(457, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "4 Bands";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(625, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "5 Bands";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // band1
+            // 
+            this.band1.BackColor = System.Drawing.Color.DimGray;
+            this.band1.Location = new System.Drawing.Point(490, 182);
+            this.band1.Name = "band1";
+            this.band1.Size = new System.Drawing.Size(16, 114);
+            this.band1.TabIndex = 6;
+            this.band1.Click += new System.EventHandler(this.band1_Click);
+            // 
+            // band5
+            // 
+            this.band5.BackColor = System.Drawing.Color.DimGray;
+            this.band5.Location = new System.Drawing.Point(655, 182);
+            this.band5.Name = "band5";
+            this.band5.Size = new System.Drawing.Size(16, 114);
+            this.band5.TabIndex = 7;
+            this.band5.Click += new System.EventHandler(this.band5_Click);
+            // 
+            // band2
+            // 
+            this.band2.BackColor = System.Drawing.Color.DimGray;
+            this.band2.Location = new System.Drawing.Point(538, 194);
+            this.band2.Name = "band2";
+            this.band2.Size = new System.Drawing.Size(10, 93);
+            this.band2.TabIndex = 8;
+            this.band2.Click += new System.EventHandler(this.band2_Click);
+            // 
+            // band3
+            // 
+            this.band3.BackColor = System.Drawing.Color.DimGray;
+            this.band3.Location = new System.Drawing.Point(576, 194);
+            this.band3.Name = "band3";
+            this.band3.Size = new System.Drawing.Size(10, 93);
+            this.band3.TabIndex = 9;
+            this.band3.Click += new System.EventHandler(this.band3_Click);
+            // 
+            // band4
+            // 
+            this.band4.BackColor = System.Drawing.Color.DimGray;
+            this.band4.Location = new System.Drawing.Point(614, 194);
+            this.band4.Name = "band4";
+            this.band4.Size = new System.Drawing.Size(10, 93);
+            this.band4.TabIndex = 10;
+            this.band4.Click += new System.EventHandler(this.band4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(484, 308);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(31, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(521, 308);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(31, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(558, 308);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(31, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(595, 308);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(31, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(649, 308);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(30, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(685, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Ohms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 476);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.band4);
+            this.Controls.Add(this.band3);
+            this.Controls.Add(this.band2);
+            this.Controls.Add(this.band5);
+            this.Controls.Add(this.band1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ohmbg);
+            this.Name = "Form1";
+            this.Text = "Band Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label ohmbg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label band1;
+        private System.Windows.Forms.Label band5;
+        private System.Windows.Forms.Label band2;
+        private System.Windows.Forms.Label band3;
+        private System.Windows.Forms.Label band4;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.ColorDialog colorDialog3;
+        private System.Windows.Forms.ColorDialog colorDialog4;
+        private System.Windows.Forms.ColorDialog colorDialog5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FinalQuestion9/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalQuestion9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            band5.BackColor = Color.NavajoWhite;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            band5.BackColor = Color.DimGray;
+        }
+
+        private void band1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                band1.BackColor = colorDialog1.Color;
+        }
+
+        private void band2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog2.ShowDialog() == DialogResult.OK)
+                band2.BackColor = colorDialog2.Color;
+        }
+
+        private void band3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog3.ShowDialog() == DialogResult.OK)
+                band3.BackColor = colorDialog3.Color;
+        }
+
+        private void band4_Click(object sender, EventArgs e)
+        {
+            if (colorDialog4.ShowDialog() == DialogResult.OK)
+                band4.BackColor = colorDialog4.Color;
+        }
+
+        private void band5_Click(object sender, EventArgs e)
+        {
+            if (colorDialog5.ShowDialog() == DialogResult.OK)
+                band5.BackColor = colorDialog5.Color;
+        }
+    }
+}
